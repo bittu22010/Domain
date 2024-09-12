8b6109e81cdad83916b02f3cab3b47b358907f5e
Bittu kumar <kbitu2248@gmail.com>
2024-09-12 11:25:54 +0530

    Add files via upload

---
+++ ClassSpringBootSecurityApplication.java
@@ -0,0 +1,14 @@
+package ClassSpringBootSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassSpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassSpringBootSecurityApplication.class, args);
+		System.out.println("Our Student Security1 application is running...");
+	}
+
+}
